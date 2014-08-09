e53c19b16f0597afcda57831f2d62034652a7930
Adithep <badsxx@gmail.com>
2014-08-09 12:45:57 +0700

    ma

--- package.js
+++ package.js
@@ -1,16 +1,18 @@
 Package.describe({
-  summary: "REPLACEME - What does this package (or the original one you're wrapping) do?"
+  summary: "REPLACEME - What does this package (or the original one you're wrapping) do?",
+  version: "0.0.1"
 });
 
 Npm.depends({mongodb: "1.4.5"});
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
   api.use(['coffeescript', 'underscore'], ['server']);
   api.add_files('mongo-aggregation.coffee', ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use('mongo-aggregation');
+  api.use("../packages/bads:mongo-aggregation");
 
   api.add_files('mongo-aggregation_tests.js', ['client', 'server']);
 });
