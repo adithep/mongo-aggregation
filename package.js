904bee47fe49d168215aab6d91b7842dfc27e9b2
Adithep SriNarula <badsxx@gmail.com>
2014-08-25 08:34:24 +0700

    cry

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
   summary: "REPLACEME - What does this package (or the original one you're wrapping) do?",
-  version: "0.0.1"
+  version: "0.0.1",
+  name: "bads:mongo-aggregation"
 });
 
 Npm.depends({mongodb: "1.4.5"});
@@ -12,7 +13,7 @@ Package.on_use(function (api, where) {
 });
 
 Package.on_test(function (api) {
-  api.use("../packages/bads:mongo-aggregation");
+  api.use("bads:mongo-aggregation");
 
   api.add_files('mongo-aggregation_tests.js', ['client', 'server']);
 });
